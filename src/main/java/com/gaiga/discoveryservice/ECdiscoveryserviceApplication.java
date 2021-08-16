23668c066d986d1f5f4476d5b91151ca1e285937
Kidding Just <rlarkdlr@gmail.com>
2021-08-16 13:57:34 +0900

    commit

---
+++ src/main/java/com/gaiga/discoveryservice/ECdiscoveryserviceApplication.java
@@ -0,0 +1,17 @@
+package com.gaiga.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ECdiscoveryserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECdiscoveryserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaiga/discoveryservice/ECdiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaiga.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECdiscoveryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
